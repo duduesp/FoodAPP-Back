558f990c296b39d57f7b8e46c943a0b32027030c
duduesp <dudu.es@hotmail.com.ar>
2022-10-18 17:11:12 -0300

    a verrr

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PGPORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`listen port ${PGPORT}`); // eslint-disable-line no-console
   });
 });
